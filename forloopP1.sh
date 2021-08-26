7c15e1932c4235a5742e20c22e469297917715ca
Anshul722 <anshulasati722@gmail.com>
2021-08-26 16:50:21 +0530

    added: 2nd program of forloop

---
+++ forloopP1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+read -p "Enter a number" n
+power=1
+for((i=1;i<=$n;i++))
+do
+power=$(($power*2))
+done
+echo $power
---
+++ forloopP2.sh
@@ -0,0 +1,9 @@
+read -p "Enter a number" n
+num=1
+for((i=1;i<=n;i++))
+do
+   har=1
+   har=$har/$i
+   num=$num+$har
+done
+   echo $num
